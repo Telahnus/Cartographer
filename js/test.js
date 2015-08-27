b5dc73a3650b9a83d790cfd6ffa16ed6f72588e4
Telahnus <quan.jordan@gmail.com>
2015-08-27 15:02:07 -0400

    Here comes another attempt
    Wiping the slate clean once again

--- js/test.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-	$("#load_btn").click(function(){
-		var mapid = $("#mapid").val();
-		//$("#testdiv").text("localhost/frame.php?map="+mapid);
-		$("#iframe").attr("src","/frame.php?map="+mapid);
-		//$("#frame").attr("src", "http://www.google.com/");
-	});
-}); 
