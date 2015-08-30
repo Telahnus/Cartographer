c8bad3149cd988db2895a74dae8ec00172b00d31
Telahnus <quan.jordan@gmail.com>
2015-08-29 23:07:08 -0400

    simple map
    just setting up the demo map project

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Simple Map</title>
+    <meta name="viewport" content="initial-scale=1.0">
+    <meta charset="utf-8">
+    <script src="js/index.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/index.css">
+    <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBjA0w2FVebqiZZzgAlrHJDIvn5YmeX5dY&callback=initMap" ></script>
+  </head>
+
+  <body>
+    <div id="map"></div>
+  </body>
+
+</html>
+
